87780f3f8f8ea2ebfa18fc17c3d7eccbc7334e9c
Maxim Zavitaev <vskweb@gmail.com>
2015-07-16 02:20:14 +0300

    Настройка подключения к БД

--- index.php
+++ index.php
@@ -12,6 +12,14 @@ $fw->mset(array(
     'site.url' => $fw->get('SCHEME') . '://' . $fw->get('HOST') . $fw->get('BASE') . '/'
 ));
 
+$fw->config('app/config/settings.ini');
 $fw->config('app/config/routes.ini');
+
+$fw->set('db.instance', new \DB\SQL(
+    'mysql:host=' . $fw->get('db.host') . ';port=' . $fw->get('db.port') . ';dbname=' . $fw->get('db.name'),
+    $fw->get('db.user'),
+    $fw->get('db.pass')
+));
+\Helper\Settings::instance();
 
 $fw->run();
